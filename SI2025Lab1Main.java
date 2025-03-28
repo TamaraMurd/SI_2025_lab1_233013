8bc3eafb7d4e4b7d01e6a7df958189674576bfaa
TamaraMurd <tamaramurdzoska711@gmail.com>
2025-03-28 23:38:37 +0100

    Adding 2- find all completed tasks

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -76,9 +76,9 @@ public void removeTask(String name) {
 }
 
     // 2. Find all completed tasks
-    public List<Task> getCompletedTasks() {
+   public List<Task> getCompletedTasks() {
         // TODO: Implement logic to return completed tasks
-        return new ArrayList<>();
+        return tasks.stream().filter(Task::isCompleted).collect(Collectors.toList());
     }
 
     // 3. List tasks sorted by name
